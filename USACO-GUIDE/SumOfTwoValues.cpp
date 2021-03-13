0984ca8b2cba76ecb4284e18abab0f30f8e2e40c
bhargavannem <bhargav.annem@gmail.com>
2021-03-13 14:57:46 -0600

    solves all cases but 1

---
+++ USACO-GUIDE/SumOfTwoValues.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, x;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    int l = 0, r = n - 1;
+    while(l < r){
+        if(arr[l] + arr[r] == x){
+            
+        }
+    }
+
+}
---
+++ USACO-GUIDE/palsquare.cpp
@@ -0,0 +1,13 @@
+/*
+ID: bhargav22
+LANG: C++
+TASK: palsquare
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream fin("palsquare.in");
+    int n; cin >> n;
+}
