0aec1cd54742d666cf0426f29a16e0a1ef9ccc50
bhargavannem <bhargav.annem@gmail.com>
2021-04-12 19:01:54 -0500

    2 solved this comp

---
+++ Codeforces/GCD_Length/test.cpp
@@ -0,0 +1,153 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll	
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef long long ll;
+typedef vector<long long> vll;
+typedef unsigned long long ull;
+typedef vector<vector<int>> vvi;
+typedef pair<int, int> pii;
+typedef pair<pair<int, int>, int> ppi;
+typedef vector<pair<int, int>> vii;
+typedef map<int, int> mii;
+typedef map<char, int> mci;
+typedef map<int, bool> mib;
+typedef map<int, string> mis;
+typedef map<string, int> msi;
+typedef map<int, char> mic;
+typedef set<int> si;
+typedef set<char> sc;
+typedef set<string> ss;
+typedef stack<int> sti;
+typedef stack<string> sts;
+typedef stack<char> stc;
+
+
+/// Extra Start 
+#define nn                    '\n' 
+#define SS                    stringstream 
+#define ull                   unsigned long long 
+#define mod                   1000000007 
+#define SIZE                  2000001 
+#define space                 " " 
+#define StringToInt           if ( ! (istringstream(s) >> n) ) n = 0; 
+/// Extra End 
+ 
+ 
+/// Functions Start 
+ template < class T > T Multiply( T a, T b ){return a * b ;} 
+template < class T > T larger( T a, T b ){return ( a > b ? a : b );} 
+template < class T > T smaller( T a, T b ){return ( a < b ? a : b );} 
+template<class T> T gcd(T a,T b){if(b == 0)return a;return gcd(b,a%b);} 
+template<class T> T lcm(T a, T b ){return (a*b)/gcd(a,b);} 
+template < class T > string converter( T n ){SS x;x << n;return x.str();}  
+/// Functions End 
+ 
+ 
+/// Debug Start 
+template<class T1> void deb(T1 e1) 
+{ 
+    cout << e1 << endl; 
+} 
+template<class T1,class T2> void deb(T1 e1, T2 e2) 
+{ 
+    cout << e1 << space << e2 << endl; 
+} 
+template<class T1,class T2,class T3> void deb(T1 e1, T2 e2, T3 e3) 
+{ 
+    cout << e1 << space << e2 << space << e3 << endl; 
+} 
+template<class T1,class T2,class T3,class T4> void deb(T1 e1, T2 e2, T3 e3, T4 e4) 
+{ 
+    cout << e1 << space << e2 << space << e3 << space << e4 << endl; 
+} 
+template<class T1,class T2,class T3,class T4,class T5> void deb(T1 e1, T2 e2, T3 e3, T4 e4, T5 e5) 
+{ 
+    cout << e1 << space << e2 << space << e3 << space << e4 << space << e5 << endl; 
+} 
+template<class T1,class T2,class T3,class T4,class T5,class T6> void deb(T1 e1, T2 e2, T3 e3, T4 e4 ,T5 e5, T6 e6) 
+{ 
+    cout << e1 << space << e2 << space << e3 << space << e4 << space << e5 << space << e6 << endl; 
+} 
+/// Debug End 
+
+template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a){in>>a.fr>>a.sc;return in;}
+template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a){out<<a.fr<<" "<<a.sc;return out;}
+template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
+template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
+
+const long long INF=1e18;
+const int32_t M=1e9+7;
+const int32_t MM=998244353;
+void setIO(string s) {
+  freopen((s + ".in").c_str(), "r", stdin);
+  freopen((s + ".out").c_str(), "w", stdout);
+}
+
+pair<string, string> solve(int a, int b, int c){
+    string u(a, '0'),  v(b, '0');
+for(int i = 0; i < max(0, a - (c - 1)); i++){
+        // if(i % 2 == 1){
+        //     u[i] = (char)((i + 1) + '0');
+        // }
+        // else{
+        //     u[i] = (char)('9' - i);
+        // }
+        u[i] = '7';
+    }
+
+    for(int i = 0; i < max(0, b - (c - 1)); i++){
+        // if(i % 2 == 1){
+        //     v[i] = (char)('9' - i);
+        // }
+        // else{
+            v[i] = (char)((i + 1) + '0');
+        // }
+    }
+    if(a - c == 4 && b - c == 2){
+        u[(a - c )] = '1';
+    }
+    return make_pair(u, v);
+}
+
+int main() {
+    int n = 0;
+    for(int a = 1; a <= 9; a++){
+        for(int b = 1; b <= 9; b++){
+            for(int c = 1; c <= min(a, b); c++){
+                auto ret = solve(a,b,c);
+                string i = ret.fr;
+                string j = ret.sc;
+
+                if(i.length() != a || j.length() != b){
+                    // deb("A/B prob");
+                    n++;
+                    deb(a,b,c);
+                }
+                int u = (int)(stoi(i));
+                int v = (int)(stoi(j));
+                int ans = gcd(u, v);
+                if(to_string(ans).length() != c){
+                    // deb("GCD Prob");
+                    deb(a,b,c);
+                    n++;
+                }
+            }
+        }
+    }
+    deb(n);
+}
--- Codeforces/Keyboard/a.cpp
+++ Codeforces/Keyboard/a.cpp
@@ -1,153 +1,73 @@
 #include "bits/stdc++.h"
 using namespace std;
-#define pb                push_back
-#define ppb               pop_back
-#define pf                push_front
-#define ppf               pop_front
-#define all(x)            (x).begin(),(x).end()
-#define uniq(v)           (v).erase(unique(all(v)),(v).end())
-#define sz(x)             (int)((x).size())
-#define fr                first
-#define sc                second
-#define rep(i,a,b)        for(int i=a;i<b;i++)
-#define mem1(a)           memset(a,-1,sizeof(a))
-#define mem0(a)           memset(a,0,sizeof(a))
-#define ppc               __builtin_popcount
-#define ppcll             __builtin_popcountll	
-typedef vector<int> vi;
-typedef vector<bool> vb;
-typedef long long ll;
-typedef vector<long long> vll;
-typedef unsigned long long ull;
-typedef vector<vector<int>> vvi;
-typedef pair<int, int> pii;
-typedef pair<pair<int, int>, int> ppi;
-typedef vector<pair<int, int>> vii;
-typedef map<int, int> mii;
-typedef map<char, int> mci;
-typedef map<int, bool> mib;
-typedef map<int, string> mis;
-typedef map<string, int> msi;
-typedef map<int, char> mic;
-typedef set<int> si;
-typedef set<char> sc;
-typedef set<string> ss;
-typedef stack<int> sti;
-typedef stack<string> sts;
-typedef stack<char> stc;
+int n, m, X;
 
+string board[51];
+vector<pair<int, int> >  shifts;
 
-/// Extra Start 
-#define nn                    '\n' 
-#define SS                    stringstream 
-#define ull                   unsigned long long 
-#define mod                   1000000007 
-#define SIZE                  2000001 
-#define space                 " " 
-#define StringToInt           if ( ! (istringstream(s) >> n) ) n = 0; 
-/// Extra End 
- 
- 
-/// Functions Start 
- template < class T > T Multiply( T a, T b ){return a * b ;} 
-template < class T > T larger( T a, T b ){return ( a > b ? a : b );} 
-template < class T > T smaller( T a, T b ){return ( a < b ? a : b );} 
-template<class T> T gcd(T a,T b){if(b == 0)return a;return gcd(b,a%b);} 
-template<class T> T lcm(T a, T b ){return (a*b)/gcd(a,b);} 
-template < class T > string converter( T n ){SS x;x << n;return x.str();}  
-/// Functions End 
- 
- 
-/// Debug Start 
-template<class T1> void deb(T1 e1) 
-{ 
-    cout << e1 << endl; 
-} 
-template<class T1,class T2> void deb(T1 e1, T2 e2) 
-{ 
-    cout << e1 << space << e2 << endl; 
-} 
-template<class T1,class T2,class T3> void deb(T1 e1, T2 e2, T3 e3) 
-{ 
-    cout << e1 << space << e2 << space << e3 << endl; 
-} 
-template<class T1,class T2,class T3,class T4> void deb(T1 e1, T2 e2, T3 e3, T4 e4) 
-{ 
-    cout << e1 << space << e2 << space << e3 << space << e4 << endl; 
-} 
-template<class T1,class T2,class T3,class T4,class T5> void deb(T1 e1, T2 e2, T3 e3, T4 e4, T5 e5) 
-{ 
-    cout << e1 << space << e2 << space << e3 << space << e4 << space << e5 << endl; 
-} 
-template<class T1,class T2,class T3,class T4,class T5,class T6> void deb(T1 e1, T2 e2, T3 e3, T4 e4 ,T5 e5, T6 e6) 
-{ 
-    cout << e1 << space << e2 << space << e3 << space << e4 << space << e5 << space << e6 << endl; 
-} 
-/// Debug End 
-
-template<typename T1,typename T2>istream& operator>>(istream& in,pair<T1,T2> &a){in>>a.fr>>a.sc;return in;}
-template<typename T1,typename T2>ostream& operator<<(ostream& out,pair<T1,T2> a){out<<a.fr<<" "<<a.sc;return out;}
-template<typename T,typename T1>T amax(T &a,T1 b){if(b>a)a=b;return a;}
-template<typename T,typename T1>T amin(T &a,T1 b){if(b<a)a=b;return a;}
-
-const long long INF=1e18;
-const int32_t M=1e9+7;
-const int32_t MM=998244353;
-void setIO(string s) {
-  freopen((s + ".in").c_str(), "r", stdin);
-  freopen((s + ".out").c_str(), "w", stdout);
+int dist(int x1, int y1, int x2, int y2){
+    return (abs(x2 - x1) * abs(x2 - x1) + abs(y2 - y1) * abs(y2 - y1));
 }
 
-int contains(vector<char> arr, char c){
-    for(int i = 0; i < sz(arr); i++){
-        if(arr[i] == c) return i;
-    }
-    return -1;
-}
 
 void solve(){
-    char c; cin >> c;
-    string s; cin >> s;
+    scanf("%d%d%d", &n,&m,&X);
+    for(int i = 0; i < n; i++) cin >> board[i];
+    int ans = 0;
 
-    vector<char> top = {'q', 'w', 'e', 'r', 't', 'y','u','i','o','p','[',']','\\'};
-    vector<char> mid = {'a','s','d','f','g','h','j','k','l',';','\''};
-    vector<char> bot = {'z','x','c','v','b','n','m',',','.','/'};
+    int len;
+    cin >> len;
+    string s;
+    cin >> s;
 
-    string ans(s.length(), '?');
-
-    for(int i = 0; i < s.length(); i++){
-        if(contains(top, s[i]) != -1){
-            int k = contains(top, s[i]);
-            if(c == 'R'){
-                ans[i] = top[k - 1];
-            }
-            else{
-                ans[i] = top[k + 1];
-            }
-        }
-        else if(contains(mid, s[i]) != -1){
-            int k = contains(mid, s[i]);
-            if(c == 'R'){
-                ans[i] = mid[k - 1];
-            }
-            else{
-                ans[i] = mid[k + 1];
-            }
-        }
-        else if(contains(bot, s[i]) != -1){
-            int k = contains(bot, s[i]);
-            if(c == 'R'){
-                ans[i] = bot[k - 1];
-            }
-            else{
-                ans[i] = bot[k + 1];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(board[i][j] == 'S'){
+                shifts.push_back(make_pair(i, j));
             }
         }
     }
-    deb(ans);
 
+
+    for(int t = 0; t < len; t++){
+        char cur = s[t];
+        bool didLowerCase = false;
+        if(cur < 97){
+            didLowerCase = true;
+            cur += 32;
+        }
+        int x = -1,y = -1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == cur){
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        if((x == -1 || y == -1)){
+            cout << (-1) << "\n";
+            return;
+        }
+        else if((shifts).size() == 0 && didLowerCase){
+            cout << (-1) << "\n";
+            return;   
+        }
+        else if(cur >= 97 && !didLowerCase){
+            continue;
+        }
+        int low = dist(x,y,shifts[0].first, shifts[0].second);
+        for(auto i: shifts){
+            int k = dist(x,y,i.first, i.second);
+            low = min(low, k);
+        }
+        if(X * X < low){
+            ans++;
+        }
+    }
+    cout << (ans) << "\n";
 }
-signed main(){
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     // setIO();
