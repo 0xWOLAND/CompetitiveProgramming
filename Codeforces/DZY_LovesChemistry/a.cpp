b48109e4e1480a6fa6c0314f1e1f290f412c5bfb
bhargavannem <bhargav.annem@gmail.com>
2021-04-28 09:59:04 -0500

    solved

---
+++ Codeforces/DZY_LovesChemistry/a.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+typedef long long ll;
+vector<vector<long long> > x;
+bool vis[105];
+ll ans = 1;
+
+void dfs(ll n, ll pa){
+    if(n != pa){
+        ans *= 2;
+    }
+
+    vis[n] = 1;
+    for(int i = 0; i < x[n].size(); i++){
+        ll lol = x[n][i];
+        if(!vis[lol]){
+            dfs(lol, pa);
+        }
+    }
+}
+
+int main() {
+    ll n, m;
+    cin >> n >> m;
+    x.resize(n + 5);
+    for(int i = 0; i < m; i++){
+        ll a, b;
+        cin >> a >> b;
+        x[a].push_back(b);
+        x[b].push_back(a);
+    }
+
+    for(int i = 1; i <= n; i++){
+        if(!vis[i]){
+            dfs(i, i);
+        }
+    }
+
+    cout << ans << "\n";
+}
